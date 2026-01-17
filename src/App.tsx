318bde385479ed80d745682b881499f11e71518e
RoxanaCamara <roxana.camara07@gmail.com>
2026-01-17 00:30:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Title } from "./components/title";
+
+function App() {
+  return (
+    <div>
+      <Title />
+
+      <p className="how-to-play">How to play</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/title.tsx
@@ -0,0 +1,9 @@
+export const Title = () => {
+  return (
+    <div className="title">
+      <h2 className="title-the">The</h2>
+      <h1 className="title-hangman">Hangman</h1>
+      <h2 className="title-game">Game</h2>
+    </div>
+  );
+};
