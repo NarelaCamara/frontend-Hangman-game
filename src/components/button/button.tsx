017ab6e8dd6c527f551d34a873f468739bca674c
RoxanaCamara <roxana.camara07@gmail.com>
2026-02-02 01:18:51 -0300

    feat: button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export const Button = ({ text }: { text: string }) => {
   return (
     <div className="button">
-      <p className="button__text">{text}</p>
+      <p className="button__text">{String(text).toLocaleUpperCase()}</p>
     </div>
   );
 };
