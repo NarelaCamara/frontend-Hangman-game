e30fac7240a67b9f72654ef380c170e9436f96af
RoxanaCamara <roxana.camara07@gmail.com>
2026-02-19 23:05:50 -0300

    feat: letters

--- src/components/letter-alphabet/letter-alphabet.tsx
+++ src/components/letter-alphabet/letter-alphabet.tsx
@@ -9,12 +9,6 @@ export const LetterAlphabet = ({
   lettersClicked?: string[];
   onClick: (letter: string) => void;
 }) => {
-  console.log(
-    " LetterAlphabet, ",
-    letter,
-    lettersClicked,
-    lettersClicked?.some((e) => e === letter),
-  );
   return (
     <div
       className={`${lettersClicked?.some((e) => e === letter) ? "letter__alphabet--clicked" : "letter__alphabet"}`}
--- src/components/letter-word/letter-word.tsx
+++ src/components/letter-word/letter-word.tsx
@@ -7,14 +7,8 @@ export const LetterWord = ({
   letter: string;
   lettersClicked: string[];
 }) => {
-  console.log(
-    "LetterWord, ",
-    letter,
-    lettersClicked,
-    lettersClicked.some((e) => e === letter),
-  );
   return letter === " " ? (
-    <div className="letter__space"></div>
+    <div className="letter__word__space"></div>
   ) : (
     <div
       className={` ${lettersClicked.some((e) => e === letter) ? "letter__word" : "--letter__word__not__show"}`}
